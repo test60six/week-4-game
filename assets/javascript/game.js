5ca6f29f6e4b1ff80ecbae7cb276fa213d9d6380
test60six <test60six@gmail.com>
2017-08-08 21:47:57 -0400

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,53 +1,52 @@
 var randomNumber = Math.floor((Math.random() * 120) + 1);
 $("#number").text(randomNumber);
 var totalPoints = 0;
-var cNumber1;
-var cNumber2;
-var cNumber3;
-var cNumber4;
 var wins = 0
 var lose = 0
+var crystals = $(".crystal");
 
+// Make our randomization a function for re use
 function makeRandom() {
    return Math.floor((Math.random() * 12) + 1);
 }
 
-var crystals = $(".crystal");
-
-for (var i = 0; i < crystals.length; i++){
-	var $this = crystals.eq(i);
-	$this.val(makeRandom());
+// Make our randomize of our crystals into a function so we can re use it
+function randomizeCrystals() {
+  
+  for (var i = 0; i < crystals.length; i++){
+    var $this = crystals.eq(i);
+    $this.val(makeRandom());
+  }
 }
 
-function add (num){
-	totalPoints += Number(num);
+function add (num) {
+  totalPoints += Number(num);
+  $("#score").text(totalPoints);
+}
 
-	$("#score").text(totalPoints);
-
+function reset(){
+  totalPoints = 0;
+  randomNumber = Math.floor((Math.random() * 120) + 1);
+  $("#score").text(totalPoints);
+  $("#number").text(randomNumber);
+  randomizeCrystals();
 }
 
 crystals.on('click', function(event) {
-	var numberVal = Number ($(this).val());
-	add(numberVal);
-
-	if (randomNumber === totalPoints ){
-		wins++;
-		$("#wins").text(wins)
-
-		reset()
-
-
-	}
-
-	else if (totalPoints > randomNumber){
-		console.log("butts")
-		lose++;
-		$("#losses").text(lose)
-
-		reset()
-
-	}
-
-
+  var numberVal = Number($(this).val());
+  add(numberVal);
+  if (randomNumber === totalPoints ) {
+      wins++;
+      $("#wins").text(wins)
+      reset();
+  } else if (totalPoints > randomNumber){
+      console.log("butts")
+      lose++;
+      $("#losses").text(lose);
+      reset();
+  }
 });
 
+// This is our initial start of our game
+// This function will fire only once on page load
+reset(); // <--- now its reset instead of randomizeCrystals
